19c39c30705321f2d65b36d07aadd006eadef8c5
bethanyshoun <bethanyshoun@gmail.com>
2022-05-21 07:52:40 -0400

    linked js and css files to hmtl and created password length window prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 // Assignment code here
-
+var writePassword = function () {
+  //function to determine password length
+  var passwordLength = window.prompt("How many characters should your password have? Choose a number from 8 to 128.");
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +19,29 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//If click on the button to generate a password
+
+//Then prompoted for password critera
+
+//If prompted for password criteria
+
+//Then slect which criteria to include
+
+//If prompted for length of password
+
+//Then choose a length from 8-128 characters
+
+//IF asked for character types to include
+
+//Then choose from any/some/all of lowercase, uppercase, numeric and/or special characters
+
+//If I answer each prompt
+
+//Then my input should be validated and at least one character type selelcted
+
+//If all prompts are answered
+
+//Then a password is generated to match selected criteria
+
+//Password is displayed in an alert or written on the page
