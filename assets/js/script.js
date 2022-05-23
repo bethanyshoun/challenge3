3c89a237254eda1626cbbdd87c25f809ad93adda
bethanyshoun <bethanyshoun@gmail.com>
2022-05-23 07:00:15 -0400

    restored orignal write password function to end of script- from tester/comparision code

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,9 +80,11 @@ var generatePassword = function () {
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-  // Add event listener to generate button
-generateBtn.addEventListener("click", function() {
+// Write password to the #password input
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-})
+}
+  // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword); 
